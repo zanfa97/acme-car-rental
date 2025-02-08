76785aa291c24dda05f1d785c475665b52b22598
zanfa97 <zanfardinomarco97@gmail.com>
2025-02-08 11:39:25 +0100

    add rental Model

---
+++ rental-service/src/main/java/org/acme/rental/Rental.java
@@ -0,0 +1,19 @@
+package org.acme.rental;
+
+import java.time.LocalDate;
+
+public record Rental(Long id,
+                     String userId,
+                     Long reservationId,
+                     LocalDate startDate) {
+
+    @Override
+    public String toString() {
+        return "Rental{" +
+                "id=" + id +
+                ", userId='" + userId + '\'' +
+                ", reservationId=" + reservationId +
+                ", startDate=" + startDate +
+                '}';
+    }
+}
